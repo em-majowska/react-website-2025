229509a3d400c38771bc0e8ca9e4688568b22050
Ewa Majowska <em.majowska@gmail.com>
2025-11-26 20:42:25 +0400

    Swap logo for svg file

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import heroImage from '../assets/images/hero-image.png';
+import heroImage from '/assets/images/hero-image.png';
 
 const Hero = ({
   title = (
--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,19 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/react-website-2025',
-  plugins: [react(), tailwindcss()],
-  server: {
-    port: 3000,
-  },
+export default defineConfig((command) => {
+  if (command === 'build') {
+    return {
+      base: '/react-website-2025/',
+      plugins: [react(), tailwindcss()],
+    };
+  } else {
+    return {
+      base: '/',
+      plugins: [react(), tailwindcss()],
+      server: {
+        port: 3000,
+      },
+    };
+  }
 });
