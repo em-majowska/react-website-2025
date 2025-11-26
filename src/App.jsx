bf5abc46817450f9b16b21e2234eeabaa9e9f3a7
Ewa Majowska <em.majowska@gmail.com>
2025-11-26 20:44:08 +0400

    Add card templates

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import NavBar from './components/NavBar';
 import Hero from './components/Hero';
+import HomeCards from './components/HomeCards';
 
 const App = () => {
   return (
     <>
       <NavBar />
       <Hero />
+      <HomeCards />
     </>
   );
 };
